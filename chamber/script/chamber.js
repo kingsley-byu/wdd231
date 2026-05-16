d22cb0daf1040195b09b11dae7e093ffd1ac4652
kingsley-byu <osaruyiosas@gmail.com>
2026-05-16 18:53:48 +0100

    made changes to the js file and added css styling

--- chamber/script/chamber.js
+++ chamber/script/chamber.js
@@ -1,5 +1,14 @@
+const menu = document.querySelector('#menu-btn');
+const navBar = document.querySelector('#nav-bar');
+menu.addEventListener('click', () => {
+    navBar.classList.toggle('open');
+    menu.classList.toggle('open');
+});
+
 const cardContainer = document.querySelector('#cards');
-async function getMembersData(){
+const display = document.querySelector('article');
+
+async function getMembersData() {
     const response = await fetch('data/members.json');
     const data = await response.json();
     console.table(data.members);
@@ -8,52 +17,44 @@ async function getMembersData(){
 getMembersData();
 
 const displayMembers = (members) => {
-    members.forEach((member) =>{
+    members.forEach((member) => {
         let card = document.createElement('section');
 
-         // create picture element
         let picture = document.createElement('picture');
 
-       // Large screen source
         let sourceLarge = document.createElement('source');
         sourceLarge.setAttribute('srcset', `${member.imageFile}-large.webp`);
         sourceLarge.setAttribute('media', '(min-width: 1000px)');
 
-        // medium screen source
         let sourceMedium = document.createElement('source');
         sourceMedium.setAttribute('srcset', `${member.imageFile}-medium.webp`);
         sourceMedium.setAttribute('media', '(min-width: 640px)');
 
-        //Fallback image and img into picture
         let image = document.createElement('img');
-        image.setAttribute('src',`${member.imageFile}-small.webp`);
+        image.setAttribute('src', `${member.imageFile}-small.webp`);
         image.setAttribute('alt', `portrait of ${member.companyName}`);
         image.setAttribute('loading', 'lazy');
         image.setAttribute('width', '200');
         image.setAttribute('height', '150');
 
-        //Append source and image
         picture.appendChild(sourceLarge);
         picture.appendChild(sourceMedium);
         picture.appendChild(image);
 
-
-        
         let name = document.createElement('h2');
         let address = document.createElement('p');
         let phone = document.createElement('p');
         let websiteUrl = document.createElement('p');
         let level = document.createElement('p');
         let year = document.createElement('p');
-        
-        name.textContent = `${member.companyName}`;
+
+        name.textContent = member.companyName;
         address.textContent = `${member.companyAddress.street}, ${member.companyAddress.city}, ${member.companyAddress.country}`;
-        phone.textContent = `${member.phoneNumber}`;
+        phone.textContent = member.phoneNumber;
         websiteUrl.innerHTML = `<a href="${member.website}" target="_blank">Visit Website</a>`;
-        level.textContent = `${member.membershipLevel}`;
-        year.textContent = `${member.yearEstablished}`;
-        
-        // Append everything into card
+        level.textContent = member.membershipLevel;
+        year.textContent = member.yearEstablished;
+
         card.appendChild(picture);
         card.appendChild(name);
         card.appendChild(address);
@@ -62,7 +63,22 @@ const displayMembers = (members) => {
         card.appendChild(level);
         card.appendChild(year);
 
-        //Append card into container
         cardContainer.appendChild(card);
-    })
-}
+    });
+};
+
+const gridButton = document.querySelector('#grid');
+const listBtn = document.querySelector('#list');
+
+gridButton.addEventListener('click', () => {
+    display.classList.add('grid');
+    display.classList.remove('list');
+});
+
+listBtn.addEventListener('click', () => {
+    display.classList.add('list');
+    display.classList.remove('grid');
+});
+
+document.querySelector('#currentyear').textContent = new Date().getFullYear();
+document.querySelector('#lastModified').textContent = `Last Modified: ${document.lastModified}`;
