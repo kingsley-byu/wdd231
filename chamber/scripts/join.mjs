c50ec7600ad7a0ad8cd0d8bca4e6646161918206
kingsley-byu <osaruyiosas@gmail.com>
2026-05-31 13:29:18 +0100

    added dialog function

--- chamber/scripts/join.mjs
+++ chamber/scripts/join.mjs
@@ -10,15 +10,50 @@ async function fetchMemberLevelData() {
 
 
 const membershipCard = document.querySelector("#membership-card")
+const dialog = document.querySelector("#membership-dialog");
+
+
+
 function levelCards(levels) {
     levels.forEach((level) => {
         let h2 = document.createElement("h2");
-        h2.innerHTML = `<h2>Non `
+        let p = document.createElement("p");
+        h2.textContent = level.title
+        p.textContent = "Learn more"
+        
 
 
         membershipCard.appendChild(h2);
-    })
+        membershipCard.appendChild(p);
+    });
 }
+
+function dialogBox(x) {
+    dialog.innerHTML = `<button id="closeModal">❌</button>
+    <h2>${x.title}</h2>
+    <p><strong>Benefits</strong>: ${x.benefits.join(',')}</p>
+    <p><strong>Cost</strong>: ${x.cost}`;
+
+    dialog.showModal();
+
+    const closeBtn = document.querySelector("#closeModal");
+    closeBtn.addEventListener("click", () => {
+        dialog.close();
+    })
+
+} 
+
+p.addEventListener("click", (event) => {
+    const clickedCard = event.target;
+    const joinInfo = clickedCard.textContent;
+
+    const selectedLevel = data.find(level => `${level.title} ${level.id}` === joinInfo );
+    if (selectedLevel) {
+        dialogBox(selectedLevel);
+    }
+});
+
+
 
 
 fetchMemberLevelData();
